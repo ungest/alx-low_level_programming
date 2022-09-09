b1e7aa035bf0f5df94eea76264698da240fa378d
David Usoro <usorodavid1@gmail.com>
2022-09-08 21:07:22 -0700

    [puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
+
+
 /**
+ * main - Entry point
  *
- * main - print the string in the put function
+ * Return: Always 0 (Success)
  *
- * Description: It uses the main function
- * the program prints the exact text programming is like building a multilingual puzzle
- * Return: 0
  */
 
 int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
-}
 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
