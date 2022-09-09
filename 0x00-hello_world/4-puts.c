2dfcf53cf695fd426c07d14a75669d54c2a447c8
David Usoro <usorodavid1@gmail.com>
2022-09-08 20:42:27 -0700

    puts command

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ *This program prints a simple text
+ *
+ *The program uses puts function instead of printf function for output display
+ *
+ * The program will end with the output zero
+ *
+ */
+ 
+ int main(void)
+ {
+ 	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return 0;
+
+ }
