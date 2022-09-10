d6e0b90267c8ecca8ceb0c7b085d81e66655b3a6
David Usoro <usorodavid1@gmail.com>
2022-09-09 20:03:29 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	char alp;
 
-	for (alp ='a' ; alp <= 'z' ; alp++)
-		if (alp !='e' && alp != 'q')
+	for (alp = 'a' ; alp <= 'z' ; alp++)
+		if (alp != 'e' && alp != 'q')
 		{
 			putchar(alp);
 		}
