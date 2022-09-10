9a82ed94b75b00ea04b5ba803d057d445a987ddd
David Usoro <usorodavid1@gmail.com>
2022-09-09 18:52:28 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Prints the alphabets in lower case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alp;
+	for (alp = 'a' ; alp <= 'z' ; alp++)
+{
+	putchar(alp);
+}
+	putchar('\n');
+	return (0);
+}
+
