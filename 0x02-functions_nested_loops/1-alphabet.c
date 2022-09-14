f4c68bbd605c20195bcfb204b2741010a6a3cca3
David Usoro <usorodavid1@gmail.com>
2022-09-14 11:03:56 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets in lowercase
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar('j');
+	}
+	_putchar('\n');
+}
+
+
+
